b6935bab7c815739ac8cbbd465b45e1c7fc97386
Luis <luisdiazvenero@gmail.com>
2018-09-04 17:04:46 -0500

    UI de formularios, caja de busqueda

--- src/categories/components/categories.js
+++ src/categories/components/categories.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Category from './category';
 import './categories.css';
+import SearchContainer from '../../widgets/containers/search';
 
 function Categories(props){
   return(
     <div className="Categories">
+      <SearchContainer></SearchContainer>
       {
         props.categories.map((item)=>{
           return (
             <Category
               key={item.id}
               {...item}
-              handleOpenModal={props.handleOpenModal} 
+              handleOpenModal={props.handleOpenModal}
             />
           )
         })
---
+++ src/widgets/components/search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './search.css';
+
+const Search = () => (
+  <form className="Search">
+    <input
+      className="Search-input"
+      placeholder="Busca tu video favorito"
+      type="text"></input>
+  </form>
+)
+
+export default Search;
