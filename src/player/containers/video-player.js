83787f837237dc505501870a6ff15ffc4a669f0a
Luis <luisdiazvenero@gmail.com>
2018-09-05 16:02:17 -0500

    componente de titulo y fuente de video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
+import Video from '../components/video.js';
+import Title from '../components/title.js'
 
 class VideoPlayer extends Component {
   render(){
     return(
       <VideoPlayerLayout>
-        <video
-          src="http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4"
-          autoPlay
-          controls
-          >
-        </video>
+        <Title
+          title="Esto es un video chill"
+          />
+        <Video
+          autoplay={true}
+          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+           />
       </VideoPlayerLayout>
     )
   }
